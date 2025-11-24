e4c2659294a907df5f4e2013383506270077896e
Sin0n0me <zakokamikami@icloud.com>
2025-11-24 16:25:48 +0900

    update: エラーのバリアントを増やした

--- compiler/nagi_driver/src/errors.rs
+++ compiler/nagi_driver/src/errors.rs
@@ -1,8 +1,11 @@
 use std::{error::Error, fmt::Display, io};
 
+use nagi_command_option::errors::CommandOptionError;
+
 #[derive(Debug)]
 pub(crate) enum CompileError {
     IOError(io::Error),
+    CommandOptionError(CommandOptionError),
     Other(String), // 専用のエラーができるまで
 }
 
@@ -13,8 +16,9 @@ impl Error for CompileError {}
 impl Display for CompileError {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
-            CompileError::IOError(e) => write!(f, "{}", e),
-            CompileError::Other(e) => write!(f, "{}", e),
+            CompileError::IOError(e) => write!(f, "{e}"),
+            CompileError::CommandOptionError(e) => write!(f, "{}", e.message),
+            CompileError::Other(e) => write!(f, "{e}"),
         }
     }
 }
@@ -22,6 +26,12 @@ impl Display for CompileError {
 impl From<io::Error> for CompileError {
     fn from(value: io::Error) -> Self {
         CompileError::IOError(value)
+    }
+}
+
+impl From<CommandOptionError> for CompileError {
+    fn from(value: CommandOptionError) -> Self {
+        CompileError::CommandOptionError(value)
     }
 }
 
